5994ff82b8d61bdb6fa4466082c4d8c381466c1e
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-04-22 02:50:11 +0000

    almost working 4/21/22 9:50

--- src/App.java
+++ src/App.java
@@ -7,6 +7,10 @@ public class App {
         Scanner scan = new Scanner(new File("nitin.dat"));
         ArrayList<String> cats = new ArrayList<String>();
         ArrayList<String> dogs = new ArrayList<String>();
+        ArrayList<String> allPeople = new ArrayList<String>();
+        ArrayList<String> allPeopleTypes = new ArrayList<String>();
+        ArrayList<String> allPetsTypes = new ArrayList<String>();
+        ArrayList<String> allPets = new ArrayList<String>();        
         ArrayList<String> catPerson = new ArrayList<String>();
         ArrayList<String> dogPerson = new ArrayList<String>();
         int animalLoop = scan.nextInt();
@@ -14,23 +18,67 @@ public class App {
         for (int i = 0; i < animalLoop; i++) {
             String petName = scan.next();
             String petType = scan.next();
-            System.out.println(petType);
+
             if (petType.equals("D")) {
                 dogs.add(petName);
             }
             if (petType.equals("C")) {
                 cats.add(petName);
             }
-
-           System.out.println(dogs);
-           System.out.println(cats);
+            allPets.add(petName);
+            allPetsTypes.add(petType);
         }
 
         int peopleLoop = scan.nextInt();
 
         for (int i = 0; i < peopleLoop; i++) {
+            String personName = scan.next();
+            String personType = scan.next();
+
+            if (personType.equals("D")) {
+                dogPerson.add(personName);
+            }
+            if (personType.equals("C")) {
+                catPerson.add(personName);
+            }
+            allPeople.add(personName);
+            allPeopleTypes.add(personType);
 
         }
+
+        
+        for(int i = 0; i < allPeople.size(); i++){
+            //checks for all of the arrays
+            String outputString = "";
+            if(allPeopleTypes.get(i).equals("C")){
+                outputString+=allPeople.get(i);
+                outputString = outputString + " CAT ";
+                for(int j = 0; j<cats.size(); j++){
+                    if(!(cats.get(j) == null)){
+                        outputString+=cats.get(j);
+                        cats.set(j, null);
+                        break;
+                    }
+                }
+                
+
+            }
+            if(allPeopleTypes.get(i).equals("D")){
+                outputString+=allPeople.get(i);
+                outputString = outputString + " DOG ";
+                for(int j = 0; j<dogs.size(); j++){
+                    if(!(dogs.get(j) == null)){
+                        outputString+=dogs.get(j);
+                        dogs.set(j, null);
+                        break;
+                    }
+                }
+            }
+            System.out.println(outputString);
+        }
+
+
+
 
     }
 }
